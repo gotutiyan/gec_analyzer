ac3c99901394ba0f987db4707aec68f6c44c8bb4
gotutiyan <gto.rikujyo@icloud.com>
2024-08-14 01:51:28 +0900

    add streamlit sample

---
+++ p.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import errant
+from annotated_text import annotated_text
+import argparse
+
+ETYPE = [
+    'ADJ',
+    'ADV',
+    'CONJ',
+    'CONTR',
+    'DET',
+    'NOUN',
+    'NOUN:POSS',
+    'OTHER',
+    'PART',
+    'PREP',
+    'PRON',
+    'PUNCT',
+    'VERB',
+    'VERB:FORM',
+    'VERB:TENSE',
+    'ADJ:FORM',
+    'MORPH',
+    'NOUN:INFL',
+    'NOUN:NUM',
+    'ORTH',
+    'SPELL',
+    'VERB:INFL',
+    'VERB:SVA',
+    'WO'
+]
+
+def gen_annotate_tokens(tokens, edits, etype2visible):
+    annotated_tokens = []
+    idx = 0
+    for e in edits:
+        no_edited_tokens = tokens[idx:e.o_start]
+        if no_edited_tokens != '':
+            annotated_tokens.append(' '.join(no_edited_tokens) + ' ')
+        mru_type = e.type[0]
+        other_type = ':'.join(e.type.split(':')[1:])
+        if etype2visible[mru_type] and etype2visible[other_type]:
+            annotated_tokens.append((
+                f'{e.o_str} -> {e.c_str}' if e.o_str != '' else f'φ -> {e.c_str}',
+                e.type
+            ))
+        else:
+            annotated_tokens.append(
+                f'{e.o_str} -> {e.c_str}' if e.o_str != '' else f'φ -> {e.c_str}',
+            )
+        idx = e.o_end
+    if idx < len(tokens):
+        no_edited_tokens = tokens[idx:]
+        annotated_tokens.append(' '.join(no_edited_tokens) + ' ')
+    return annotated_tokens
+
+def main(args):
+    st.header('Input sentences')
+    srcs = st.text_area(
+        'Input Sources',
+        value='This are gramamtical sentence .\nThis are gramamtical sentence .'
+    ).rstrip().split('\n')
+    hyps = st.text_area(
+        'Input Hypotheses',
+        value='This is grammatical sentence .\nThis is grammatical sentence .'
+    ).rstrip().split('\n')
+    refs = st.text_area(
+        'Input References',
+        value='This is a grammatical sentence .\nThis is a grammatical sentence .'
+    ).rstrip().split('\n')
+    etype2visible = dict()
+    st.header('Select visible error types')
+    for etype in ['M', 'R', 'U']:
+        etype2visible[etype] = st.checkbox(etype, value=True)
+    st.divider()
+    for etype in ETYPE:
+        etype2visible[etype] = st.checkbox(etype, value=True)
+    annotator = errant.load('en')
+    p_srcs = [annotator.parse(s) for s in srcs]
+    p_hyps = [annotator.parse(h) for h in hyps]
+    p_refs = [annotator.parse(r) for r in refs]
+
+    hyp_edits = [annotator.annotate(s, h) for s, h in zip(p_srcs, p_hyps)]
+    ref_edits = [annotator.annotate(s, t) for s, t in zip(p_srcs, p_refs)]
+
+    n_sents = len(srcs)
+    for i in range(n_sents):
+        st.header(f'Line {i+1}')
+        st.write(f'<strong>Source:</strong> {srcs[i]}', unsafe_allow_html=True)
+        st.markdown('#### References')
+        tokens = srcs[i].split(' ')
+        annotated_tokens = gen_annotate_tokens(
+            tokens,
+            ref_edits[i],
+            etype2visible
+        )
+        annotated_text(annotated_tokens)
+        st.markdown('#### Hypotheses')
+        tokens = srcs[i].split(' ')
+        annotated_tokens = gen_annotate_tokens(
+            tokens,
+            hyp_edits[i],
+            etype2visible
+        )
+        annotated_text(annotated_tokens)
+        st.divider()
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = get_parser()
+    main(args)
+
